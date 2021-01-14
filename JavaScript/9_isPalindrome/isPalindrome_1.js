03b596945d7d5f50f7f6cd5851eeec098db3d773
yym-yumeng123 <18026493873@163.com>
2021-01-15 00:27:43 +0800

    修改文件夹 & 添加解题思路

--- JavaScript/9_isPalindrome/isPalindrome_1.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-  if(x < 0) return false
-  return x.toString() === x.toString().split('').reverse().join('')
-};
